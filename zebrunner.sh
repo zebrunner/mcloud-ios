751c8ec23cbd0978b519e6bc63331424e1e03356
vdelendik <vdelendik@solvd.com>
2020-12-22 04:31:24 +0300

    #21: reorganized startWDA.sh script

--- zebrunner.sh
+++ zebrunner.sh
@@ -84,7 +84,6 @@
     fi
   }
 
-
   start-appium() {
     udid=$1
     if [ "$udid" == "" ]; then
@@ -97,7 +96,7 @@
     . ./configs/getDeviceArgs.sh $udid
 
     if [ "${device_ip}" == "" ]; then
-      echo "Unable to start Appium for '${name}' as it's ip address not detected!" >> "${BASEDIR}/logs/appium_${name}.log"
+      echo "Unable to start Appium for '${name}' as it's ip address not detected!" >> "logs/appium_${name}.log"
       exit -1
     fi
     echo "Starting appium: ${udid} - device name : ${name}"
@@ -111,7 +110,7 @@
       --tmp "${BASEDIR}/tmp/AppiumData/${udid}" \
       --default-capabilities \
      '{"mjpegServerPort": '${mjpeg_port}', "webkitDebugProxyPort": '${iwdp_port}', "clearSystemFiles": "false", "webDriverAgentUrl":"'http://${device_ip}:${wda_port}'", "derivedDataPath":"'${BASEDIR}/tmp/DerivedData/${udid}'", "preventWDAAttachments": "true", "simpleIsVisibleCheck": "true", "wdaLocalPort": "'$wda_port'", "usePrebuiltWDA": "true", "useNewWDA": "'$newWDA'", "platformVersion": "'$os_version'", "automationName":"'${automation_name}'", "deviceName":"'$name'" }' \
-      --nodeconfig ./metaData/$udid.json >> "${BASEDIR}/logs/appium_${name}.log" 2>&1 &
+      --nodeconfig ./metaData/$udid.json >> "logs/appium_${name}.log" 2>&1 &
 
   }
 
@@ -126,7 +125,7 @@
     . configs/getDeviceArgs.sh $udid
 
     if [ "${device_ip}" == "" ]; then
-      echo "Unable to start STF for '${name}' as it's ip address not detected!" >> "${BASEDIR}/logs/stf_${name}.log"
+      echo "Unable to start STF for '${name}' as it's ip address not detected!" >> "logs/stf_${name}.log"
       exit -1
     fi
 
@@ -153,10 +152,49 @@
       --connect-app-dealer tcp://${STF_PRIVATE_HOST}:7160 --connect-dev-dealer tcp://${STF_PRIVATE_HOST}:7260 \
       --wda-host ${device_ip} --wda-port ${wda_port} \
       --appium-host ${STF_NODE_HOST} --appium-port ${appium_port} --proxy-appium-port ${proxy_appium_port} \
-      --connect-sub tcp://${STF_PRIVATE_HOST}:7250 --connect-push tcp://${STF_PRIVATE_HOST}:7270 --no-cleanup >> "${BASEDIR}/logs/stf_${name}.log" 2>&1 &
+      --connect-sub tcp://${STF_PRIVATE_HOST}:7250 --connect-push tcp://${STF_PRIVATE_HOST}:7270 --no-cleanup >> "logs/stf_${name}.log" 2>&1 &
 
   }
 
+  start-wda() {
+    udid=$1
+    if [ "$udid" == "" ]; then
+      echo_warning "You have to provide device udid: ./zebrunner.sh start-wda udid"
+      echo_telegram
+      exit -1
+    fi
+    #echo udid: $udid
+
+    . ./configs/getDeviceArgs.sh $udid
+
+    #backup current wda log to be able to analyze failures if any
+    if [[ -f logs/${name}_wda.log ]]; then
+      mv logs/${name}_wda.log backup/${name}_wda_`date +"%T"`.log
+    fi
+
+    echo Starting WDA: ${name}, udid: ${udid}, wda_port: ${wda_port}, mjpeg_port: ${mjpeg_port}
+    nohup /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -project ${appium_home}/node_modules/appium-webdriveragent/WebDriverAgent.xcodeproj \
+      -derivedDataPath "${BASEDIR}/tmp/DerivedData/${udid}" \
+      -scheme WebDriverAgentRunner -destination id=$udid USE_PORT=$wda_port MJPEG_SERVER_PORT=$mjpeg_port test > "logs/${name}_wda.log" 2>&1 &
+
+    verifyWDAStartup "logs/${name}_wda.log" 120 >> "logs/${name}_wda.log"
+    if [[ $? = 0 ]]; then
+      # WDA was started successfully!
+      # parse ip address from log file line:
+      # 2020-07-13 17:15:15.295128+0300 WebDriverAgentRunner-Runner[5660:22940482] ServerURLHere->http://192.168.88.127:20001<-ServerURLHere
+
+      ip=`grep "ServerURLHere->" "${BASEDIR}/logs/${name}_wda.log" | cut -d ':' -f 5`
+      # remove forward slashes
+      ip="${ip//\//}"
+      # put IP address into the metadata file
+      echo "${ip}" > ${metaDataFolder}/ip_${udid}.txt
+
+    else
+      # WDA is not started successfully!
+      rm -f ${metaDataFolder}/ip_${udid}.txt
+    fi
+
+  }
 
   stop() {
     if [ ! -f backup/settings.env ]; then
@@ -168,7 +206,6 @@
     stop-wda
     stop-stf
     stop-appium
-
   }
 
   stop-wda() {
@@ -380,6 +417,63 @@
     fi
   }
 
+  verifyWDAStartup() {
+
+    ## FUNCTION:     verifyStartup
+    ## DESCRITION:   verify if WDA component started per device/simolator
+    ## PARAMETERS:
+    ##         $1 - Path to log file for startup verification
+    ##         $2 - String to find in startup log (startup indicator)
+    ##         $3 - Counter. (Startup verification max duration) = (Counter) x (10 seconds)
+
+    STARTUP_LOG=$1
+    STARTUP_COUNTER=$2
+
+    STARTUP_INDICATOR="ServerURLHere->"
+    FAIL_INDICATOR=" TEST FAILED "
+    UNSUPPORTED_INDICATOR="Unable to find a destination matching the provided destination specifier"
+
+    COUNTER=0
+    while [  $COUNTER -lt $STARTUP_COUNTER ];
+    do
+      sleep 1
+      if [[ -r ${STARTUP_LOG} ]]
+      then
+        # verify that WDA is supported for device/simulator
+        grep "${UNSUPPORTED_INDICATOR}" ${STARTUP_LOG} > /dev/null
+        if [[ $? = 0 ]]
+        then
+          echo "ERROR! WDA does not support ${name}!"
+          return -1
+        fi
+
+        # verify that WDA failed
+        grep "${FAIL_INDICATOR}" ${STARTUP_LOG} > /dev/null
+        if [[ $? = 0 ]]
+        then
+          echo "ERROR! WDA failed on ${name} in ${COUNTER} seconds!"
+          return -1
+        fi
+
+       grep "${STARTUP_INDICATOR}" ${STARTUP_LOG} > /dev/null
+        if [[ $? = 0 ]]
+        then
+          echo "WDA started successfully on ${name} within ${COUNTER} seconds."
+          return 0
+        else
+          echo "WDA not started yet on ${name}. waiting ${COUNTER} sec..."
+        fi
+
+      else
+        echo "ERROR! Cannot read from ${STARTUP_LOG}. File has not appeared yet!"
+      fi
+      let COUNTER=COUNTER+1
+    done
+
+    echo "ERROR! WDA not started on ${name} within ${STARTUP_COUNTER} seconds!"
+    return -1
+  }
+
   echo_warning() {
     echo "
       WARNING! $1"
@@ -436,6 +530,9 @@ case "$1" in
         ;;
     start-stf)
         start-stf $2
+        ;;
+    start-wda)
+        start-wda $2
         ;;
     stop)
         stop
