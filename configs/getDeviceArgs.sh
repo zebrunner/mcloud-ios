c3dd2b67ce9bcbf5a0cf5424ef5a55bfa88b3673
vdelendik <vdelendik@zebrunner.com>
2022-01-29 16:40:18 +0300

    declared DEVICE_UDID for better apium integration

--- configs/getDeviceArgs.sh
+++ configs/getDeviceArgs.sh
@@ -19,7 +19,8 @@ export type=$(echo $type)
 os_version=`cat ${devices} | grep "$udid" | cut -d '|' -f 3`
 export os_version=$(echo $os_version)
 
-# no need to read udid as it is provided as required argument
+DEVICE_UDID=`cat ${devices} | grep "$udid" | cut -d '|' -f 1`
+export DEVICE_UDID=$(echo $DEVICE_UDID)
 
 appium_port=`cat ${devices} | grep "$udid" | cut -d '|' -f 5`
 export appium_port=$(echo $appium_port)
