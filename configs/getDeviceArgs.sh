a198fcdc8f4d7ff40e339e11b21b6e0dc850c4d0
vdelendik <vdelendik@zebrunner.com>
2022-01-29 16:42:36 +0300

    fixed DEVICE_UDID parser position

--- configs/getDeviceArgs.sh
+++ configs/getDeviceArgs.sh
@@ -22,7 +22,7 @@ export type=$(echo $type)
 os_version=`cat ${devices} | grep "$udid" | cut -d '|' -f 3`
 export os_version=$(echo $os_version)
 
-DEVICE_UDID=`cat ${devices} | grep "$udid" | cut -d '|' -f 1`
+DEVICE_UDID=`cat ${devices} | grep "$udid" | cut -d '|' -f 4`
 export DEVICE_UDID=$(echo $DEVICE_UDID)
 
 appium_port=`cat ${devices} | grep "$udid" | cut -d '|' -f 5`
