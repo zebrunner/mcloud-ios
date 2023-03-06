6995fc3d5ade13ee9a5c8c6addf17efe56363d6a
idmcloud <idmcloud@idmclouds-Mac-mini.local>
2023-03-07 00:34:23 +0300

    reuse develop branch of stf with 2.4.4 updates

--- zebrunner.sh
+++ zebrunner.sh
@@ -197,7 +197,7 @@ export SIMULATORS=${metaDataFolder}/simulators.txt
 
     echo
     echo "Pull STF updates:"
-    stf_branch=2.4.3
+    stf_branch=develop
     if [ ! -d stf ]; then
       git clone https://github.com/zebrunner/stf.git
       cd stf
