6c4dead5eeece090d3aeaf114e23552da8cdcc2e
idmcloud <idmcloud@idmclouds-Mac-mini.local>
2023-03-11 16:45:05 +0300

    #215: bump up to stf:2.4.4

--- zebrunner.sh
+++ zebrunner.sh
@@ -197,7 +197,7 @@ export SIMULATORS=${metaDataFolder}/simulators.txt
 
     echo
     echo "Pull STF updates:"
-    stf_branch=develop
+    stf_branch=2.4.4
     if [ ! -d stf ]; then
       git clone https://github.com/zebrunner/stf.git
       cd stf
